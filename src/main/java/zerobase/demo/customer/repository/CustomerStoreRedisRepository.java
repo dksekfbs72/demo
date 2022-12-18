9b8e272e57dbd1b1052da6a04644841e4d4e0099
김세종 <ksj2083@naver.com>
2022-12-19 03:23:31 +0900

    임시 주석처리

--- src/main/java/zerobase/demo/customer/repository/CustomerStoreRedisRepository.java
+++ src/main/java/zerobase/demo/customer/repository/CustomerStoreRedisRepository.java
@@ -1,11 +1,11 @@
-package zerobase.demo.customer.repository;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-import zerobase.demo.customer.dto.CustomerStoreInfo;
-
-public interface CustomerStoreRedisRepository
-	extends CrudRepository<List<CustomerStoreInfo>, CustomerStoreInfo.ListParam> {
-}
+// package zerobase.demo.customer.repository;
+//
+// import java.util.List;
+//
+// import org.springframework.data.repository.CrudRepository;
+//
+// import zerobase.demo.customer.dto.CustomerStoreInfo;
+//
+// public interface CustomerStoreRedisRepository
+// 	extends CrudRepository<List<CustomerStoreInfo>, CustomerStoreInfo.ListParam> {
+// }
