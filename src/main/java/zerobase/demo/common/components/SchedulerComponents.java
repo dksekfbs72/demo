3cbf3a9fe5831789ccaed3d752bf83ef28d024f6
dksekfbs72 <dksekfbs72@naver.com>
2022-12-23 01:32:21 +0900

    리뷰 홍보 메일 전송 spring-batch 변경
    sql 패키지의 sql을 db에서 실행하고 돌려야 오류가 안납니다.

--- src/main/java/zerobase/demo/common/components/SchedulerComponents.java
+++ src/main/java/zerobase/demo/common/components/SchedulerComponents.java
@@ -1,45 +1,38 @@
 package zerobase.demo.common.components;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import zerobase.demo.common.entity.Order;
-import zerobase.demo.common.entity.Review;
-import zerobase.demo.common.entity.User;
-import zerobase.demo.common.type.OrderStatus;
-import zerobase.demo.order.repository.OrderRepository;
-import zerobase.demo.owner.repository.StoreRepository;
-import zerobase.demo.user.repository.UserRepository;
+import zerobase.demo.common.config.JobConfig;
 
 @Slf4j
 @Component
 @AllArgsConstructor
 public class SchedulerComponents {
-	private final MailComponents mailComponents;
-	private final OrderRepository orderRepository;
-	private final UserRepository userRepository;
 
+	private JobLauncher jobLauncher;
+	private JobConfig jobConfig;
 	@Scheduled(cron = "0 0 0/1 * * *")
 	public void promotingReviewEmailSender() {
 		log.info("send email to promote review");
-
-		List<Order> orderList = orderRepository.findAllByOrderTimeBetweenAndReviewedAndStatus(
-			LocalDateTime.now().minusHours(2),
-			LocalDateTime.now().minusHours(1),
-			false,
-			OrderStatus.DELIVERY_COMPLETE);
-
-
-		for (Order i : orderList) {
-			mailComponents.sendMail(userRepository.findById(i.getUserId()).get().getUserId(),
-									"[조기요] 리뷰를 남겨보세요!",
-									"<p>주문에 감사드립니다.</p>"
-										+ "<p>주문하신 음식에 대한 리뷰를 남겨보세요!</p>");
+		Map<String, JobParameter> confMap = new HashMap<>();
+		confMap.put("time", new JobParameter(System.currentTimeMillis()));
+		JobParameters jobParameters = new JobParameters(confMap);
+		try {
+			jobLauncher.run(jobConfig.Job(), jobParameters);
+		} catch (JobInstanceAlreadyCompleteException | JobParametersInvalidException |
+				 JobExecutionAlreadyRunningException | JobRestartException e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
